3ed8d75856a742a8bd15707e2a3161fe5825446b
“Jakub <jakub@jkan.pl>
2020-11-02 21:33:43 +0100

    To jest fajny KOMIT

--- src/main/java/pl/jkanclerz/pp5/App.java
+++ src/main/java/pl/jkanclerz/pp5/App.java
@@ -1,7 +1,25 @@
 package pl.jkanclerz.pp5;
 
+import pl.jkanclerz.pp5.factory.Employee;
+import pl.jkanclerz.pp5.factory.FactoryConfiguration;
+import pl.jkanclerz.pp5.factory.Product;
+
+import java.util.Collections;
+
+
 public final class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Employee employee = FactoryConfiguration.getResponsibleEmployee();
+
+        Product p1 = employee.doWork();
+        Product p2 = employee.doWork();
+        Product p3 = employee.doWork();
+
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
     }
 }
---
+++ src/main/java/pl/jkanclerz/pp5/factory/Boss.java
@@ -0,0 +1,17 @@
+package pl.jkanclerz.pp5.factory;
+import java.util.List;
+
+class Boss implements Employee {
+    private final List<Worker> workers;
+
+    Boss(List<Worker> workers) {
+        this.workers = workers;
+    }
+
+    @Override
+    public Product doWork() {
+        return workers.stream().findFirst()
+                .get()
+                .doWork();
+    }
+}
